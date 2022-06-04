3bb59f6c03d7bbab88da77f8b6ed7a2ee477ae06
Auleen <43134470+Auleen@users.noreply.github.com>
2022-06-05 01:33:44 +0530

    Added Google Pay Button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Footer.css";
+import GooglePayButton from "@google-pay/button-react";
+
 function Footer(props) {
   return (
     <div className="footer fixed-bottom">
@@ -14,7 +16,48 @@ function Footer(props) {
       <div className="total">
         <strong>Total Bill:</strong> ${props.total}
       </div>
-      <button className="pay">Pay Now</button>
+      {/* <button className="pay">Pay Now</button> */}
+      <div className="paybutton">
+        <GooglePayButton
+          environment="TEST"
+          buttonColor="white"
+          paymentRequest={{
+            apiVersion: 2,
+            apiVersionMinor: 0,
+            buttonColor: "white",
+            allowedPaymentMethods: [
+              {
+                type: "CARD",
+                parameters: {
+                  allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                  allowedCardNetworks: ["MASTERCARD", "VISA"],
+                },
+                tokenizationSpecification: {
+                  type: "PAYMENT_GATEWAY",
+                  parameters: {
+                    gateway: "example",
+                    gatewayMerchantId: "exampleGatewayMerchantId",
+                  },
+                },
+              },
+            ],
+            merchantInfo: {
+              merchantId: "12345678901234567890",
+              merchantName: "Demo Merchant",
+            },
+            transactionInfo: {
+              totalPriceStatus: "FINAL",
+              totalPriceLabel: "Total",
+              totalPrice: "100.00",
+              currencyCode: "USD",
+              countryCode: "US",
+            },
+          }}
+          onLoadPaymentData={(paymentRequest) => {
+            console.log("load payment data", paymentRequest);
+          }}
+        />
+      </div>
     </div>
   );
 }
