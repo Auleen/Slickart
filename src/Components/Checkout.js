a38cc09a9fcb0dc2d3569f85463f1c3e018feb47
Pious <auleenghosh.sg@gmail.com>
2022-06-09 11:33:38 +0530

    Added Checkout Page

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,9 +1,139 @@
 import React, { Component } from "react";
-function Checkout() {
-  console.log("Checkout");
+import { useLocation } from "react-router-dom";
+import GooglePayButton from "@google-pay/button-react";
+function Checkout(props) {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
-      <div>This is the checkout page </div>
+      <div className="container mt-5">
+        <div>Thank You for shopping at Slickart ✨ </div>
+        <div>Your Total is ${props.total}</div>
+        <hr></hr>
+        <div>
+          <strong>Fill in the your Shippinng Details</strong>
+        </div>
+        <form className="row g-3 mt-2">
+          <div className="col-md-6">
+            <label htmlFor="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword4"
+            />
+          </div>
+          <div className="col-12">
+            <label htmlFor="inputAddress" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+            />
+          </div>
+          <div className="col-12">
+            <label htmlFor="inputAddress2" className="form-label">
+              Address 2
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress2"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputCity" className="form-label">
+              City
+            </label>
+            <input type="text" className="form-control" id="inputCity" />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="inputState" className="form-label">
+              State
+            </label>
+            <select
+              id="inputState"
+              className="form-select"
+              // placeholder="Select State"
+            >
+              <option selected="">Delhi</option>
+              <option>Punjab</option>
+              <option>Uttar Pradesh</option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="inputZip" className="form-label">
+              Zip
+            </label>
+            <input type="text" className="form-control" id="inputZip" />
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="gridCheck"
+              />
+              <label className="form-check-label" htmlFor="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="paybutton">
+              <GooglePayButton
+                environment="TEST"
+                buttonColor="white"
+                paymentRequest={{
+                  apiVersion: 2,
+                  apiVersionMinor: 0,
+                  allowedPaymentMethods: [
+                    {
+                      type: "CARD",
+                      parameters: {
+                        allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                        allowedCardNetworks: ["MASTERCARD", "VISA"],
+                      },
+                      tokenizationSpecification: {
+                        type: "PAYMENT_GATEWAY",
+                        parameters: {
+                          gateway: "example",
+                          gatewayMerchantId: "exampleGatewayMerchantId",
+                        },
+                      },
+                    },
+                  ],
+                  merchantInfo: {
+                    merchantId: "12345678901234567890",
+                    merchantName: "Demo Merchant",
+                  },
+                  transactionInfo: {
+                    totalPriceStatus: "FINAL",
+                    totalPriceLabel: "Total",
+                    totalPrice: "100.00",
+                    currencyCode: "USD",
+                    countryCode: "US",
+                  },
+                }}
+                onLoadPaymentData={(paymentRequest) => {
+                  console.log("load payment data", paymentRequest);
+                }}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
