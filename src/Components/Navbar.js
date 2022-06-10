96756d066ca1545b24fee9cb8e09d15c76515b72
Pious <auleenghosh.sg@gmail.com>
2022-06-10 09:15:05 +0530

    Navbar Updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 class Navbar extends Component {
   state = {};
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Slickart
+          <a className="navbar-brand">
+            <Link style={{ textDecoration: "none", color: "aliceblue" }} to="/">
+              Slickart
+            </Link>
           </a>
         </div>
       </nav>
